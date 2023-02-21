29e9a4372ec8d89aa7c572ce4f22eb23c0bfa004
Anna Nechaeva <annanechaeva9595@gmail.com>
2023-02-21 15:37:04 +1000

    Задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        // Задача: Дан массив пар названий книг и авторов, необходимо привести каждую пару
+        // в строку вида: «Название книги» И.О.Автора.
 
+        String[] books = {"Война и мир", "Евгений Онегин", "Идиот"};
+        String[] authors = {"Л.Н.Толстой", "А.С.Пушкин", "Ф.М.Достоевский"};
+
+        for (int i = 0; i < books.length && i < authors.length; i++) {
+            String book = "«" + books[i] + "» " + authors[i];
+            System.out.println(book);
+        }
     }
 }
